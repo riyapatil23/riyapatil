708f234edf9d8e3c7126a2522159e01b41e8adcb
riyapatil23 <riyapatil2301@gmail.com>
2025-07-21 22:11:00 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+  public static void main(String[] args) {
+        System.out.println("Jai Shreeyg Ganesh...!");
+         int a = 10;
+         int b = 20;
+         int c = ++b + a-- + a++ + b;
+         System.out.println(c);
+
+    }
+}
