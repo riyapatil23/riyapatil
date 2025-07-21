f85d2d820056ae9520ddaa3d9f67dce1d5ce2bd5
riyapatil23 <riyapatil2301@gmail.com>
2025-07-21 21:38:42 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,14 @@
+public class Pattern5 {
+    public static void main(String[] args) {
+   //     *
+   //   * 
+   // *
+for(int row = 1; row <=5; row++) {
+  for(int col = row; col <= 5; col++) {
+    System.out.print(" " );
+  }
+  System.out.println("*");
+} 
+    }
+    
+}
