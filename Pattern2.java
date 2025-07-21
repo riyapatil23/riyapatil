ba12411711064e9e103f3c076788cb012a16815b
riyapatil23 <riyapatil2301@gmail.com>
2025-07-21 21:14:11 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,15 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        // ***** 
+      // *****
+      // *****
+       int num = 10;
+        for (int i = 1; i < num; i++) {
+           for (int j = i; j > 5 ;j++) {
+            System.out.print("*");
+           }
+            System.out.println(" ");
+       } 
+    }
+    
+}
